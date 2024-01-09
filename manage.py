09cb7776dd67869955fe01aa3b4e5564d3b0860e
YulianaKaravaeva <93388991+YulianaKaravaeva@users.noreply.github.com>
2024-01-09 13:48:20 +0000

    Sorted

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-# import os
+import os
 
-# from app import create_app
+from app import create_app
 
-# app = create_app()
-# app.config.from_object("config.DevelopmentConfig")
+app = create_app()
+app.config.from_object("config.DevelopmentConfig")
 
-# if __name__ == '__main__':
-#   app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", debug=True)
